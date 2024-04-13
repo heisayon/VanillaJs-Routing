1be4ff9b77b672d0cd1eb0178510c0d7d5ba0c99
Zion <heisayon@gmail.com>
2024-04-13 01:22:01 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const mainPage = document.getElementById("app");
+console.log("hello world");
+const route = (event) => {
+  event = window.event || event;
+  event.preventDefault();
+  window.history.pushState(null, null, event.target.href);
+  handleLocation();
+};
+
+const routes = {
+  404: "/404.html",
+  "/": "/index.html",
+  "/about": "/about.html",
+  "/bye": "/bye.html",
+};
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+  const data = await fetch(route).then((data) => data.text());
+  mainPage.innerHTML = data;
+};
+
+window.route = route;
+window.onpopstate = handleLocation;
+
+handleLocation();
